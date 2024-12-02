fa2297e039832b4ffa79a641c57115de27be80b2
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-12-02 22:20:37 +0530

    Navbar design completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import cart_icon from "../../assets/cart_icon.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menu, setMenu] = useState("shop");
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          SHOP {menu == "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          MEN {menu == "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          WOMEN {menu == "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          KIDS {menu == "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>LOGIN</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
