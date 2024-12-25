1ea743e3ae24218919fa2bdf7a6ebc041667a59b
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-12-25 10:05:26 +0530

    Shop Category Page completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import LoginSignup from "./pages/LoginSignup";
 import Footer from "./components/Footer/Footer";
+import men_banner from "./assets/banner_mens.png";
+import women_banner from "./assets/banner_women.png";
+import kid_banner from "./assets/banner_kids.png";
 
 const App = () => {
   return (
@@ -15,9 +18,18 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category="men" />} />
-          <Route path="/women" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/men"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kids" />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
